321aa2036e04282e3277b60fe876f08db687d9a0
xiaocong <xc1642906756@gmail.com>
2025-09-28 22:57:56 +0800

    查看购物车业务开发代码

--- sky-server/src/main/java/com/sky/controller/user/ShopController.java
+++ sky-server/src/main/java/com/sky/controller/user/ShopController.java
@@ -1,6 +1,7 @@
 package com.sky.controller.user;
 
 
+import com.sky.entity.ShoppingCart;
 import com.sky.result.Result;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -8,6 +9,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController("userShopController")
 @RequestMapping("/user/shop")
@@ -32,4 +35,5 @@ public class ShopController {
         log.info("获取店铺营业状态：{}",status == 1 ? "营业中" : "打样中");
         return Result.success(status);
     }
+
 }
